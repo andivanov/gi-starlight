0737000bd9e79d180ccdb169169d237e8481d712
Andrei Ivanov <andrei@getinsured.com>
2014-09-23 16:57:12 -0700

    adding new files to the project

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,78 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.registerTask('default', [ 'less', 'imagemin', 'cssmin',/*'concat','uglify',*/'watch' ]);
 
-        concat: {
-            sass: {
-                dist: {
-                    options: {
-                        style: 'compressed'
-                    },
-                    files: {
-                        'css/main.css': 'sass/main.scss'
-                    }
-                } 
+  grunt.initConfig({
+    notify_hooks: {
+        options: {
+          enabled: true,
+          max_jshint_notifications: 5, // maximum number of notifications from jshint output
+          title: "PHIX" // defaults to the name in package.json, or will use project directory's name
+        }
+      },
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'img/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest:'img/minified/'
             }
         }
+    },
+    less: {
+        style: {
+            files: {
+                "css/global.css": "less/global.less"
+            }
+        }
+    },
+   /* concat: {
+        dist: {
+          src: [
+                'css/jquery-ui-1.8.20.custom.min.css', 
+                'css/jquery.ui.datepicker.min.css', 
+                'css/bootstrap-3.1.1.min.css',
+                'css/phix-home-animate.css',
+                'css/phix-home-animation-hover.css',
+                'css/phix-home-animation-slide.css'
+            ],
+          dest: 'css/global.css',
+        },
+      }, */
+    cssmin: {
+        combine: {
+            files: {
+                'css/global.min.css': ['css/global.css']
+            }
+        }
+    },
+    /*uglify: {
+        my_target: {
+            files: {
+                'js/gihome.min.js': ['js/gihome.js'],
+                'js/accessibility.min.js': ['js/accessibility.js']
+            }
+        }
+    },*/
+    watch: {
+      css: {
+        files: ['less/*.less','css/*.css','js/*.js', 'img/*.{png,jpg,gif}'],
+        tasks: ['less:style', 'concat:dist','cssmin:combine', 'uglify:my_target', 'imagemin:dynamic'],
+        options: {
+            livereload: true,
+        }
+      }
+    }
+  });
 
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
-
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.task.run('notify_hooks');
 };
